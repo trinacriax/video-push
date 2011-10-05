6ca902cea4390bba1302908f2c8c2a040933e3b3
Alessandro Russo <russo@disi.unitn.it>
2011-10-05 16:33:53 +0200

    minor fix

--- examples/video-streaming.cc
+++ examples/video-streaming.cc
@@ -23,6 +23,7 @@
 #include "ns3/inet-socket-address.h"
 #include "ns3/video-helper.h"
 #include "ns3/wifi-module.h"
+#include "ns3/mobility-module.h"
 
 using namespace ns3;
 
@@ -43,47 +44,68 @@ main (int argc, char *argv[])
 
 	if (verbose)
 	{
-		LogComponentEnable ("StreamingFirstExample",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
-		LogComponentEnable ("VideoPushApplication",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
-		LogComponentEnable ("UdpSocketImpl",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
-		LogComponentEnable ("UdpL4Protocol",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
-		LogComponentEnable ("Ipv4EndPointDemux",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
-		LogComponentEnable ("Ipv4L3Protocol",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
-		LogComponentEnable ("Ipv4Interface",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
-		LogComponentEnable ("ArpL3Protocol",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
-		LogComponentEnable ("Socket",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
-		LogComponentEnable ("CsmaNetDevice",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+	LogComponentEnable ("StreamingFirstExample",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+	LogComponentEnable ("VideoPushApplication",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("UdpSocketImpl",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("UdpL4Protocol",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("Ipv4EndPointDemux",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("Ipv4L3Protocol",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("Ipv4Interface",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("ArpL3Protocol",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("Socket",  LogLevel( LOG_LEVEL_ALL | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("WifiNetDevice", LogLevel( LOG_LEVEL_ALL | LOG_DEBUG | LOG_LOGIC | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("AdhocWifiMac", LogLevel( LOG_LEVEL_ALL | LOG_DEBUG | LOG_LOGIC | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("DcaTxop", LogLevel( LOG_LEVEL_ALL | LOG_DEBUG | LOG_LOGIC | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("DcaManager", LogLevel( LOG_LEVEL_ALL | LOG_DEBUG | LOG_LOGIC | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("WifiMacQueue", LogLevel( LOG_LEVEL_ALL | LOG_DEBUG | LOG_LOGIC | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("DcfManager", LogLevel( LOG_LEVEL_ALL | LOG_DEBUG | LOG_LOGIC | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("Socket", LogLevel( LOG_LEVEL_ALL | LOG_DEBUG | LOG_LOGIC | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("Node", LogLevel( LOG_LEVEL_ALL | LOG_DEBUG | LOG_LOGIC | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("MacLow", LogLevel( LOG_LEVEL_ALL | LOG_DEBUG | LOG_LOGIC | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("MacRxMiddle", LogLevel( LOG_LEVEL_ALL | LOG_DEBUG | LOG_LOGIC | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("YansWifiPhy", LogLevel( LOG_LEVEL_ALL | LOG_DEBUG | LOG_LOGIC | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("InterferenceHelper", LogLevel( LOG_LEVEL_ALL | LOG_DEBUG | LOG_LOGIC | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("YansWifiChannel", LogLevel( LOG_LEVEL_ALL | LOG_DEBUG | LOG_LOGIC | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("Packet", LogLevel( LOG_LEVEL_ALL | LOG_DEBUG | LOG_LOGIC | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
+//	LogComponentEnable ("DefaultSimulatorImpl", LogLevel( LOG_LEVEL_ALL | LOG_DEBUG | LOG_LOGIC | LOG_PREFIX_FUNC | LOG_PREFIX_TIME));
 	}
 
 	NodeContainer nodes;
 	nodes.Create(2);
 
 	WifiHelper wifi = WifiHelper::Default ();
-	wifi.SetStandard (WIFI_PHY_STANDARD_80211g);
+//	wifi.SetStandard (WIFI_PHY_STANDARD_80211g);
 
 	NqosWifiMacHelper wifiMac = NqosWifiMacHelper::Default ();
 	wifiMac.SetType ("ns3::AdhocWifiMac");
 
-	YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default ();
-
 	YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default ();
 
-	YansWifiPhyHelper phy = wifiPhy;
+	YansWifiPhyHelper phy = YansWifiPhyHelper::Default ();;
 	phy.SetChannel (wifiChannel.Create ());
 	NqosWifiMacHelper mac = wifiMac;
 
+	MobilityHelper mobility;
+	mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+	mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
+	  "MinX", DoubleValue (0.0),
+	  "MinY", DoubleValue (0.0),
+	  "DeltaX", DoubleValue (10),
+	  "DeltaY", DoubleValue (10),
+	  "GridWidth", UintegerValue (2),
+	  "LayoutType", StringValue ("RowFirst"));
+
+	mobility.Install(nodes);
+
 	NetDeviceContainer nodesDevices = wifi.Install(phy, mac, nodes);
 
-	Ipv4StaticRoutingHelper staticRouting;
-	Ipv4ListRoutingHelper listRouters;
-	listRouters.Add (staticRouting, 0);
-
+//	Ipv4StaticRoutingHelper staticRouting;
+//	Ipv4ListRoutingHelper listRouters;
+//	listRouters.Add (staticRouting, 0);
+//
 	InternetStackHelper internetRouters;
-	internetRouters.SetRoutingHelper (listRouters);
+//	internetRouters.SetRoutingHelper (listRouters);
 	internetRouters.Install (nodes);
-
-	InternetStackHelper stack;
-	stack.Install (nodes);
 
 	Ipv4AddressHelper address;
 	address.SetBase ("10.1.1.0", "255.255.255.0");
@@ -91,25 +113,25 @@ main (int argc, char *argv[])
 
 	Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 
-	InetSocketAddress dst1 = InetSocketAddress (addresses.GetAddress (0), 9999);
-	VideoHelper video = VideoHelper ("ns3::UdpSocketFactory", dst1);
-	video.SetAttribute ("OnTime", RandomVariableValue (ConstantVariable (10.0)));
-	video.SetAttribute ("OffTime", RandomVariableValue (ConstantVariable (1.0)));
+	VideoHelper video = VideoHelper ("ns3::UdpSocketFactory", InetSocketAddress (addresses.GetAddress (1), 9999));
+	video.SetAttribute ("OffTime", RandomVariableValue (ConstantVariable (10.0)));
+	video.SetAttribute ("OnTime", RandomVariableValue (ConstantVariable (1.0)));
 	video.SetAttribute ("DataRate", StringValue ("10kb/s"));
 	video.SetAttribute ("PacketSize", UintegerValue (1200));
+	video.SetAttribute ("PeerType", EnumValue (SOURCE));
+	video.SetAttribute ("Local", AddressValue (addresses.GetAddress(0)));
+	video.SetAttribute ("PeerPolicy", EnumValue (RANDOM));
+	video.SetAttribute ("ChunkPolicy", EnumValue (LATEST));
 
-	ApplicationContainer apps1 = video.Install (nodes.Get(1));
+	ApplicationContainer apps1 = video.Install (nodes.Get(0));
 	apps1.Start (Seconds (1.0));
 	apps1.Stop (Seconds (10.0));
 
-	InetSocketAddress dst2 = InetSocketAddress (addresses.GetAddress (1));
-	video = VideoHelper ("ns3::UdpSocketFactory", dst2);
-	video.SetAttribute ("OnTime", RandomVariableValue (ConstantVariable (10.0)));
-	video.SetAttribute ("OffTime", RandomVariableValue (ConstantVariable (1.0)));
-	video.SetAttribute ("DataRate", StringValue ("10kb/s"));
-	video.SetAttribute ("PacketSize", UintegerValue (1200));
+	video = VideoHelper ("ns3::UdpSocketFactory", InetSocketAddress (addresses.GetAddress (0), 9999));
+	video.SetAttribute ("OffTime", RandomVariableValue (ConstantVariable (10.0)));
+	video.SetAttribute ("OnTime", RandomVariableValue (ConstantVariable (1.0)));
 
-	ApplicationContainer apps2 = video.Install (nodes.Get(0));
+	ApplicationContainer apps2 = video.Install (nodes.Get(1));
 	apps2.Start (Seconds (1.0));
 	apps2.Stop (Seconds (10.0));
 
