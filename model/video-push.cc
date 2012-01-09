738a0c58d8b3d64ef0415aaa64374152e1b0670b
Alessandro Russo <russo@disi.unitn.it>
2012-01-09 14:28:21 +0100

    Add check on chunk reception

--- model/video-push.cc
+++ model/video-push.cc
@@ -301,8 +301,12 @@ void VideoPushApplication::HandleReceive (Ptr<Socket> socket)
       ns3::pimdm::RelayTag relayTag;
       bool rtag = packet->RemovePacketTag(relayTag);
       Ipv4Address current = Ipv4Address::ConvertFrom(m_localAddress);
+//      Ipv4Address sender = Ipv4Address::ConvertFrom(from);
+      Ipv4Mask mask ("255.255.255.0");
+      current = current.GetSubnetDirectedBroadcast(mask);
+      NS_LOG_DEBUG("Packet from "<< from << " Local "<< current << " Tag ["<< relayTag.m_sender<<","<< relayTag.m_receiver<<"] :: "<<relayTag.m_receiver.IsBroadcast());
       if(rtag && current != relayTag.m_receiver){
-    	  NS_LOG_DEBUG("Discarded:"<<current<< " ! "<< relayTag.m_receiver);
+    	  NS_LOG_DEBUG("Discarded: not for clients "<<relayTag.m_receiver);
     	  break;
       }
       if (InetSocketAddress::IsMatchingType (from))
