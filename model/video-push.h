1944f472b98f029fb4390f4dab32ce3932f10be5
Alessandro Russo <russo@disi.unitn.it>
2012-06-15 14:02:48 +0200

    move m_chunks

--- model/video-push.h
+++ model/video-push.h
@@ -189,9 +189,9 @@ private:
 	bool			m_pullActive;
 
 	NeighborsSet 	m_neighbors;		// collect neighbors
-	ChunkBuffer		m_chunks;			// current chunk buffer
 	uint32_t 		m_pullMax;			// max number of pull allowed per chunk
 
+	ChunkBuffer		m_chunks;			// current chunk buffer
 	std::map<uint32_t, uint32_t> m_duplicates; // count chunks duplicated
 	std::map<uint32_t, uint64_t> m_chunk_delay; // count chunk delay
 	std::map<uint32_t, uint32_t> m_pendingPull; // count pending pulls
