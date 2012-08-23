c00d6e1162c4fe510984d11cafa374b01838baab
Alessandro Russo <russo@disi.unitn.it>
2012-08-23 12:24:40 +0200

    Reducing timing to send pull

--- model/video-push.cc
+++ model/video-push.cc
@@ -667,7 +667,7 @@ VideoPushApplication::PeerLoop ()
 					{
 						Ipv4Address target = PeerSelection (PS_RANDOM);
 						NS_ASSERT (target != Ipv4Address::GetAny());
-						double delayv = rint(UniformVariable().GetValue (m_pullTime.GetMicroSeconds()*.01, m_pullTime.GetMicroSeconds()*.30));
+						double delayv = rint(UniformVariable().GetValue (m_pullTime.GetMicroSeconds()*.01, m_pullTime.GetMicroSeconds()*.20));
 						Time delay = Time::FromDouble(delayv, Time::US);
 						Simulator::Schedule (delay, &VideoPushApplication::SendPull, this, missed, target);
 						m_pullTimer.Schedule();
