1e03cf8de5dd7273cd4f7bd4e27dfacb50077239
Alessandro Russo <russo@disi.unitn.it>
2012-09-04 12:00:38 +0200

    Send pull immediately

--- model/video-push.cc
+++ model/video-push.cc
@@ -721,9 +721,9 @@ VideoPushApplication::PeerLoop ()
 						NS_ASSERT (target != Ipv4Address::GetAny());
 //						double delayv = rint(UniformVariable().GetValue (m_pullTime.GetMicroSeconds()*.01, m_pullTime.GetMicroSeconds()*.20));
 //						double delayv = rint(UniformVariable().GetValue (m_pullSlot.GetMicroSeconds()*.01, m_pullSlot.GetMicroSeconds()*.40));
-						double delayv = 0;
-						Time delay = Time::FromDouble (delayv, Time::US);
-						Simulator::Schedule (delay, &VideoPushApplication::SendPull, this, missed, target);
+//						double delayv = 0;
+//						Time delay = Time::FromDouble (delayv, Time::US);
+						Simulator::ScheduleNow (&VideoPushApplication::SendPull, this, missed, target);
 						m_pullTimer.Schedule();
 					}
 				}
