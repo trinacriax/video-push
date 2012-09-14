77f9bef383acf8c805c0f69744ce9f670763a70f
Alessandro Russo <russo@disi.unitn.it>
2012-09-14 13:59:10 +0200

    add assert in SendPull

--- model/video-push.cc
+++ model/video-push.cc
@@ -1326,6 +1326,7 @@ VideoPushApplication::SendPull (uint32_t chunkid, const Ipv4Address target)
 	packet->AddHeader(pull);
 	m_txPullTrace (packet);
 	NS_ASSERT( GetSlotStart() <= Simulator::Now() && (GetSlotStart() + m_pullSlot) > Simulator::Now());
+	NS_ASSERT (PullSlot());
 	NS_LOG_INFO ("Node " << GetNode()->GetId() << " sends pull to "<< target << " for chunk "<< chunkid);
 	m_socket->SendTo(packet, 0, InetSocketAddress (target, PUSH_PORT));
 }
