d7639dc8d0ed2fade1657c90a7d05ccaab0235aa
Alessandro Russo <russo@disi.unitn.it>
2013-01-18 12:07:18 +0100

    update neighbor

--- model/neighbor.h
+++ model/neighbor.h
@@ -1,7 +1,6 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
  * Copyright (c) 2011 University of Trento, Italy
- * 					  University of California, Los Angeles, U.S.A.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -19,7 +18,6 @@
  *
  * Authors: Alessandro Russo <russo@disi.unitn.it>
  *          University of Trento, Italy
- *          University of California, Los Angeles U.S.A.
  */
 
 #ifndef __NEIGHBOR_H__
@@ -27,37 +25,62 @@
 
 #include <stdint.h>
 #include "chunk-video.h"
-#include "ns3/ipv4-address.h"
-#include "ns3/nstime.h"
+#include <ns3/ipv4-address.h>
+#include <ns3/nstime.h>
 
-namespace ns3{
-
-struct Neighbor{
-	Neighbor():
-		n_address (Ipv4Address::GetAny()), n_port (0){}
-	Neighbor(Ipv4Address n, uint32_t i):
-			n_address (n), n_port (i){}
-	Ipv4Address n_address;
-	uint32_t n_port;
-	Ipv4Address GetAddress ();
-	uint32_t GetPort ();
-};
-
-static inline bool
-operator == (const Neighbor &a, const Neighbor &b){
-	return (a.n_address == b.n_address )&& (a.n_port == b.n_port);
-}
-
-static inline bool
-operator < (const Neighbor &a, const Neighbor &b){
-	return (a.n_address < b.n_address );
-}
-
-static inline std::ostream&
-operator <<(std::ostream& outStream, const Neighbor& neighbor)
+namespace ns3
 {
-	return outStream << "IP="<<neighbor.n_address<<" Port="<<neighbor.n_port;
-}
+    /**
+     * Data structure containing neighbors' information.
+     */
+    struct Neighbor
+    {
+        Neighbor () :
+            n_address(Ipv4Address::GetAny()), n_port(0)
+        {
+        }
+        Neighbor (Ipv4Address n, uint32_t i) :
+            n_address(n), n_port(i)
+        {
+        }
+        Ipv4Address n_address; /// Neighbor address.
+        uint32_t n_port;       /// Neighbor port.
 
-}
+        /**
+         *
+         * \return Neighbor address.
+         * Get neighbor address.
+         */
+
+        Ipv4Address
+        GetAddress ();
+
+        /**
+         *
+         * \return Neighbor port.
+         * Get neighbor port.
+         */
+        uint32_t
+        GetPort ();
+    };
+
+    static inline bool
+    operator == (const Neighbor &a, const Neighbor &b)
+    {
+      return (a.n_address == b.n_address) && (a.n_port == b.n_port);
+    }
+
+    static inline bool
+    operator < (const Neighbor &a, const Neighbor &b)
+    {
+      return (a.n_address < b.n_address);
+    }
+
+    static inline std::ostream&
+    operator << (std::ostream& outStream, const Neighbor& neighbor)
+    {
+      return outStream << "IP=" << neighbor.n_address << " Port=" << neighbor.n_port;
+    }
+
+} // namespace ns3
 #endif /* __NEIGHBOR_H__ */
