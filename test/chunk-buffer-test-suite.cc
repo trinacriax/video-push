afadd0eb054e8136a0dfc74a6646b106fe5e942c
Alessandro Russo <russo@disi.unitn.it>
2013-01-18 12:49:32 +0100

    update test suite

--- test/chunk-buffer-test-suite.cc
+++ test/chunk-buffer-test-suite.cc
@@ -1,3 +1,26 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2011 University of Trento, Italy
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Source:  http://www.ietf.org/rfc/rfc3973.txt
+ *
+ * Authors: Alessandro Russo <russo@disi.unitn.it>
+ *          University of Trento, Italy
+ */
+
 #include <ns3/test.h>
 #include <ns3/chunk-buffer.h>
 #include <ns3/packet.h>
@@ -6,139 +29,145 @@ namespace ns3
 {
   using namespace streaming;
 
-    class ChunkBufferTestCase : public TestCase
-    {
-      public:
-        ChunkBufferTestCase ();
-        virtual void
-        DoRun (void);
-        ChunkBuffer m_chunks;
-    };
+  class ChunkBufferTestCase : public TestCase
+  {
+    public:
+      ChunkBufferTestCase ();
+      virtual void
+      DoRun (void);
+      ChunkBuffer m_chunks;
+  };
 
-    ChunkBufferTestCase::ChunkBufferTestCase () :
-        TestCase("Check Chunk Buffer")
-    {
-    }
-    void
-    ChunkBufferTestCase::DoRun (void)
-    {
+  ChunkBufferTestCase::ChunkBufferTestCase () :
+      TestCase("Check Chunk Buffer")
+  {
+  }
+  void
+  ChunkBufferTestCase::DoRun (void)
+  {
 //	bool test = true;
-      uint32_t size = 0;
-      for (uint32_t i = 1; i < 1000; i++)
-        {
-          if (i % 100 == 0)
-            continue;
-          size++;
-          ChunkVideo cv(i, i * 1000, i + 1200, 0);
-          m_chunks.AddChunk(cv, CHUNK_RECEIVED_PUSH);
-        }
+    uint32_t size = 0;
+    for (uint32_t i = 1; i < 1000; i++)
+      {
+        if (i % 100 == 0)
+          continue;
+        size++;
+        ChunkVideo cv(i, i * 1000, i + 1200, 0);
+        m_chunks.AddChunk(cv, CHUNK_RECEIVED_PUSH);
+      }
 
-      for (uint32_t i = 1; i < 1000; i++)
-        {
-          NS_TEST_ASSERT_MSG_EQ(m_chunks.HasChunk(i), !(i%100==0), "HasChunk");
-        }
+    for (uint32_t i = 1; i < 1000; i++)
+      {
+        NS_TEST_ASSERT_MSG_EQ(m_chunks.HasChunk(i), !(i%100==0), "HasChunk");
+      }
 
-      NS_TEST_ASSERT_MSG_EQ(m_chunks.GetBufferSize(), size, "Buffer Size");
-      for (uint32_t i = 1; i < 1000; i++)
-        {
-          if (!m_chunks.HasChunk(i))
-            continue;
-          ChunkVideo *cv = m_chunks.GetChunk(i);
-          NS_TEST_ASSERT_MSG_EQ(cv->c_id, i, "ChunkID");
-          NS_TEST_ASSERT_MSG_EQ(cv->c_tstamp, i*1000, "ChunkTS");
-          NS_TEST_ASSERT_MSG_EQ(cv->c_size, i+1200, "ChunkSz");
-          NS_TEST_ASSERT_MSG_EQ(cv->c_attributes_size, 0, "ChunkAz");
-        }
-      for (uint32_t i = 1; i < 1000; i++)
-        {
-          if (!m_chunks.HasChunk(i))
-            continue;
-          if (i % 25 != 0)
-            {
-              m_chunks.DelChunk(i);
-              size--;
-            }
-        }
-      NS_TEST_ASSERT_MSG_EQ(size, m_chunks.GetBufferSize(), "BufferSize");
-    }
-
-    class ChunkBufferStateTestCase : public TestCase
-    {
-      public:
-        ChunkBufferStateTestCase ();
-        virtual void
-        DoRun (void);
-        ChunkBuffer m_chunks;
-    };
-
-    ChunkBufferStateTestCase::ChunkBufferStateTestCase () :
-        TestCase("Check Chunk Buffer")
-    {
-    }
-    void
-    ChunkBufferStateTestCase::DoRun (void)
-    {
-//	bool test = true;
-//	uint32_t size = 0;
-      for (uint32_t i = 1; i < 1000; i++)
-        {
-          if (i % 100 == 0)
-            continue;
-          ChunkVideo cv(i, i * 1000, i + 1200, 0);
-          ChunkState state = (i % 2 == 0 ? CHUNK_RECEIVED_PUSH : CHUNK_RECEIVED_PULL);
-          m_chunks.AddChunk(cv, state);
-        }
-      for (uint32_t i = 1; i < 1000; i++)
-        {
-          ChunkState state;
-          if (i % 100 == 0)
-            state = CHUNK_MISSED;
-          else if (i % 2 == 0)
-            state = CHUNK_RECEIVED_PUSH;
-          else
-            state = CHUNK_RECEIVED_PULL;
-          ChunkState current = m_chunks.GetChunkState(i);
-          NS_TEST_ASSERT_MSG_EQ(current, state, "Chunk State");
-        }
-      for (uint32_t i = 1; i < 1000; i++)
-        {
-          ChunkState state;
-          if (i % 100 == 0)
-            state = CHUNK_MISSED;
-          else if (i % 2 == 0)
-            state = CHUNK_RECEIVED_PULL;
-          else
-            state = CHUNK_RECEIVED_PUSH;
-          m_chunks.SetChunkState(i, state);
-        }
-      for (uint32_t i = 1; i < 1000; i++)
-        {
-          if (i % 25 == 0)
+    NS_TEST_ASSERT_MSG_EQ(m_chunks.GetBufferSize(), size, "Buffer Size");
+    for (uint32_t i = 1; i < 1000; i++)
+      {
+        if (!m_chunks.HasChunk(i))
+          continue;
+        ChunkVideo *cv = m_chunks.GetChunk(i);
+        NS_TEST_ASSERT_MSG_EQ(cv->c_id, i, "ChunkID");
+        NS_TEST_ASSERT_MSG_EQ(cv->c_tstamp, i*1000, "ChunkTS");
+        NS_TEST_ASSERT_MSG_EQ(cv->c_size, i+1200, "ChunkSz");
+        NS_TEST_ASSERT_MSG_EQ(cv->c_attributes_size, 0, "ChunkAz");
+      }
+    for (uint32_t i = 1; i < 1000; i++)
+      {
+        if (!m_chunks.HasChunk(i))
+          continue;
+        if (i % 25 != 0)
+          {
             m_chunks.DelChunk(i);
-        }
-      for (uint32_t i = 1; i < 1000; i++)
-        {
-          uint32_t missed = m_chunks.GetLeastMissed(0, 1);
-          if (i % 25 == 0)
-            {
-              NS_TEST_ASSERT_MSG_EQ(i, missed, "LeastMissed");
-              m_chunks.SetChunkState(i, CHUNK_SKIPPED);
-            }
-        }
-    }
+            size--;
+          }
+      }
+    NS_TEST_ASSERT_MSG_EQ(size, m_chunks.GetBufferSize(), "BufferSize");
+  }
 
-    static class ChunkBufferTestSuite : public TestSuite
-    {
-      public:
-        ChunkBufferTestSuite ();
-    } j_chunkBufferTestSuite;
+  class ChunkBufferStateTestCase : public TestCase
+  {
+    public:
+      ChunkBufferStateTestCase ();
+      virtual void
+      DoRun (void);
+      ChunkBuffer m_chunks;
+  };
 
-    ChunkBufferTestSuite::ChunkBufferTestSuite () :
-        TestSuite("chunk-buffer", UNIT)
-    {
-      // RUN $ ./test.py -s chunk-buffer-packet -v -c unit 1
-      /// ./test.py -s chunk-buffer -v -c unit 1 -w -m -g
-      AddTestCase(new ChunkBufferTestCase());
-      AddTestCase(new ChunkBufferStateTestCase());
-    }
+  ChunkBufferStateTestCase::ChunkBufferStateTestCase () :
+      TestCase("Check Chunk State")
+  {
+  }
+
+  void
+  ChunkBufferStateTestCase::DoRun (void)
+  {
+    std::cout << "init buffer" << std::endl;
+    for (uint32_t i = 1; i < 1000; i++)
+      {
+        if (i % 100 == 0)
+          continue;
+        ChunkVideo cv(i, i * 1000, i + 1200, 0);
+        ChunkState state = (i % 2 == 0 ? CHUNK_RECEIVED_PUSH : CHUNK_RECEIVED_PULL);
+        m_chunks.AddChunk(cv, state);
+      }
+    std::cout << "check state " << std::endl;
+    for (uint32_t i = 1; i < 1000; i++)
+      {
+        ChunkState state;
+        if (i % 100 == 0)
+          state = CHUNK_MISSED;
+        else if (i % 2 == 0)
+          state = CHUNK_RECEIVED_PUSH;
+        else
+          state = CHUNK_RECEIVED_PULL;
+        ChunkState current = m_chunks.GetChunkState(i);
+        NS_TEST_ASSERT_MSG_EQ(current, state, "Chunk State");
+      }
+    std::cout << "set state" << std::endl;
+    for (uint32_t i = 1; i < 1000; i++)
+      {
+        ChunkState state;
+        if (i % 100 == 0)
+          state = CHUNK_MISSED;
+        else if (i % 2 == 0)
+          state = CHUNK_RECEIVED_PULL;
+        else
+          state = CHUNK_RECEIVED_PUSH;
+        m_chunks.SetChunkState(i, state);
+      }
+    std::cout << "del buffer" << std::endl;
+    for (uint32_t i = 1; i < 1000; i++)
+      {
+        if (i % 25 == 0)
+          m_chunks.DelChunk(i);
+      }
+    std::cout << "check buffer" << std::endl;
+    for (uint32_t i = 1; i < 1000; i++)
+      {
+        ChunkState state;
+        if (i % 100 == 0 || i % 25 == 0)
+          state = CHUNK_MISSED;
+        else if (i % 2 == 0)
+          state = CHUNK_RECEIVED_PULL;
+        else
+          state = CHUNK_RECEIVED_PUSH;
+        ChunkState current = m_chunks.GetChunkState(i);
+        NS_TEST_ASSERT_MSG_EQ(current, state, "Chunk State");
+      }
+  }
+
+  static class ChunkBufferTestSuite : public TestSuite
+  {
+    public:
+      ChunkBufferTestSuite ();
+  } j_chunkBufferTestSuite;
+
+  ChunkBufferTestSuite::ChunkBufferTestSuite () :
+      TestSuite("chunk-buffer", UNIT)
+  {
+    /// ./test.py -s chunk-buffer -v -c unit 1 -w -m -g
+//    AddTestCase(new ChunkBufferTestCase());
+    AddTestCase(new ChunkBufferStateTestCase());
+  }
 } // namespace ns3
--- test/chunk-header-test-suite.cc
+++ test/chunk-header-test-suite.cc
@@ -1,7 +1,6 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
  * Copyright (c) 2011 University of Trento, Italy
- * 					  University of California, Los Angeles, U.S.A.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -20,207 +19,218 @@
  *
  * Authors: Alessandro Russo <russo@disi.unitn.it>
  *          University of Trento, Italy
- *          University of California, Los Angeles U.S.A.
  */
 
-#include "ns3/test.h"
-#include "ns3/chunk-packet.h"
-#include "ns3/packet.h"
+#include <ns3/test.h>
+#include <ns3/chunk-packet.h>
+#include <ns3/packet.h>
 
-namespace ns3 {
-
-class ChunkHeaderTestCase : public TestCase {
-public:
-  ChunkHeaderTestCase ();
-  virtual void DoRun (void);
-};
-
-ChunkHeaderTestCase::ChunkHeaderTestCase ()
-  : TestCase ("Check ChunkHeader")
-{}
-void
-ChunkHeaderTestCase::DoRun (void)
+namespace ns3
 {
-  Packet packet;
-  streaming::ChunkHeader msgIn;
-  msgIn.SetType(MSG_CHUNK);
-  msgIn.SetReserved(0);
-  msgIn.SetChecksum(61255);
-  msgIn.Print(std::cout);
-  packet.AddHeader(msgIn);
 
-  streaming::ChunkHeader msgOut;
-  packet.RemoveHeader (msgOut);
-  msgOut.Print(std::cout);
+  class ChunkHeaderTestCase : public TestCase
   {
-	NS_TEST_ASSERT_MSG_EQ(msgOut.GetType(),MSG_CHUNK,"ChunkHeader Type");
-	NS_TEST_ASSERT_MSG_EQ(msgOut.GetReserved(),0,"Reserved");
-	NS_TEST_ASSERT_MSG_EQ(msgOut.GetChecksum(),61255,"Checksum");
+    public:
+      ChunkHeaderTestCase ();
+      virtual void
+      DoRun (void);
+  };
+
+  ChunkHeaderTestCase::ChunkHeaderTestCase () :
+      TestCase("Check ChunkHeader")
+  {
+  }
+  void
+  ChunkHeaderTestCase::DoRun (void)
+  {
+    Packet packet;
+    streaming::ChunkHeader msgIn;
+    msgIn.SetType(MSG_CHUNK);
+    msgIn.SetReserved(0);
+    msgIn.SetChecksum(61255);
+    msgIn.Print(std::cout);
+    packet.AddHeader(msgIn);
+
+    streaming::ChunkHeader msgOut;
+    packet.RemoveHeader(msgOut);
+    msgOut.Print(std::cout);
+      {
+        NS_TEST_ASSERT_MSG_EQ(msgOut.GetType(), MSG_CHUNK, "ChunkHeader Type");
+        NS_TEST_ASSERT_MSG_EQ(msgOut.GetReserved(), 0, "Reserved");
+        NS_TEST_ASSERT_MSG_EQ(msgOut.GetChecksum(), 61255, "Checksum");
+      }
+
+    msgIn.SetType(MSG_PULL);
+    msgIn.SetReserved(1);
+    msgIn.SetChecksum(31225);
+    msgIn.Print(std::cout);
+    packet.AddHeader(msgIn);
+
+    packet.RemoveHeader(msgOut);
+    msgOut.Print(std::cout);
+      {
+        NS_TEST_ASSERT_MSG_EQ(msgOut.GetType(), MSG_PULL, "ChunkHeader Type");
+        NS_TEST_ASSERT_MSG_EQ(msgOut.GetReserved(), 1, "Reserved");
+        NS_TEST_ASSERT_MSG_EQ(msgOut.GetChecksum(), 31225, "Checksum");
+      }
   }
 
-  msgIn.SetType(MSG_PULL);
-  msgIn.SetReserved(1);
-  msgIn.SetChecksum(31225);
-  msgIn.Print(std::cout);
-  packet.AddHeader(msgIn);
-
-  packet.RemoveHeader (msgOut);
-  msgOut.Print(std::cout);
+  class ChunkTestCase : public TestCase
   {
-  NS_TEST_ASSERT_MSG_EQ(msgOut.GetType(),MSG_PULL,"ChunkHeader Type");
-  NS_TEST_ASSERT_MSG_EQ(msgOut.GetReserved(),1,"Reserved");
-  NS_TEST_ASSERT_MSG_EQ(msgOut.GetChecksum(),31225,"Checksum");
+    public:
+      ChunkTestCase ();
+      virtual void
+      DoRun (void);
+  };
+
+  ChunkTestCase::ChunkTestCase () :
+      TestCase("Check ChunkMessage")
+  {
   }
-}
+  void
+  ChunkTestCase::DoRun (void)
+  {
+    Packet packet;
+    streaming::ChunkHeader msgIn;
+    msgIn.SetType(MSG_CHUNK);
+    msgIn.SetReserved(0);
+    msgIn.SetChecksum(45665);
+    msgIn.Print(std::cout);
+      {
+        streaming::ChunkHeader::ChunkMessage &chunkIn = msgIn.GetChunkMessage();
+        streaming::ChunkVideo video(10, 987654321, 100, 10);
+        chunkIn.SetChunk(video);
+        chunkIn.Print(std::cout);
+      }
+    packet.AddHeader(msgIn);
 
+    streaming::ChunkHeader msgOut;
+    packet.RemoveHeader(msgOut);
+    msgOut.Print(std::cout);
+      {
+        NS_TEST_ASSERT_MSG_EQ(msgOut.GetType(), MSG_CHUNK, "ChunkHeader Type");
+        NS_TEST_ASSERT_MSG_EQ(msgOut.GetReserved(), 0, "Reserved");
+        NS_TEST_ASSERT_MSG_EQ(msgOut.GetChecksum(), 45665, "Checksum");
 
-class ChunkTestCase : public TestCase {
-public:
-	ChunkTestCase ();
-  virtual void DoRun (void);
-};
+        streaming::ChunkHeader::ChunkMessage &chunkOut = msgIn.GetChunkMessage();
+        streaming::ChunkVideo video = chunkOut.GetChunk();
+        chunkOut.Print(std::cout);
 
-ChunkTestCase::ChunkTestCase ()
-  : TestCase ("Check ChunkMessage")
-{}
-void
-ChunkTestCase::DoRun (void)
-{
-	  Packet packet;
-	  streaming::ChunkHeader msgIn;
-	  msgIn.SetType(MSG_CHUNK);
-	  msgIn.SetReserved(0);
-	  msgIn.SetChecksum(45665);
-	  msgIn.Print(std::cout);
-	  {
-		  streaming::ChunkHeader::ChunkMessage &chunkIn = msgIn.GetChunkMessage ();
-		  streaming::ChunkVideo video (10, 987654321, 100, 10);
-		  chunkIn.SetChunk(video);
-		  chunkIn.Print(std::cout);
-	  }
-	  packet.AddHeader(msgIn);
+        NS_TEST_ASSERT_MSG_EQ(video.c_id, 10, "ChunkIdentifier");
+        NS_TEST_ASSERT_MSG_EQ(video.c_tstamp, 987654321, "Timestamp");
+        NS_TEST_ASSERT_MSG_EQ(video.c_size, 100, "ChunkSize");
+        NS_TEST_ASSERT_MSG_EQ(video.c_attributes_size, 10, "ChunkAttributeSize");
+      }
+  }
 
-	  streaming::ChunkHeader msgOut;
-	  packet.RemoveHeader (msgOut);
-	  msgOut.Print(std::cout);
-	  {
-		NS_TEST_ASSERT_MSG_EQ(msgOut.GetType(),MSG_CHUNK,"ChunkHeader Type");
-		NS_TEST_ASSERT_MSG_EQ(msgOut.GetReserved(),0,"Reserved");
-		NS_TEST_ASSERT_MSG_EQ(msgOut.GetChecksum(),45665,"Checksum");
+  class PullTestCase : public TestCase
+  {
+    public:
+      PullTestCase ();
+      virtual void
+      DoRun (void);
+  };
 
-		streaming::ChunkHeader::ChunkMessage &chunkOut = msgIn.GetChunkMessage ();
-		streaming::ChunkVideo video = chunkOut.GetChunk();
-		chunkOut.Print(std::cout);
+  PullTestCase::PullTestCase () :
+      TestCase("Check PullMessage")
+  {
+  }
+  void
+  PullTestCase::DoRun (void)
+  {
+    Packet packet;
+    streaming::ChunkHeader msgIn;
+    msgIn.SetType(MSG_PULL);
+    msgIn.SetReserved(1);
+    msgIn.SetChecksum(65535);
+    msgIn.Print(std::cout);
+      {
+        streaming::ChunkHeader::PullMessage &chunkIn = msgIn.GetPullMessage();
+        chunkIn.SetChunk(5);
+        chunkIn.Print(std::cout);
+      }
+    packet.AddHeader(msgIn);
 
-		NS_TEST_ASSERT_MSG_EQ (video.c_id, 10, "ChunkIdentifier");
-		NS_TEST_ASSERT_MSG_EQ (video.c_tstamp, 987654321, "Timestamp");
-		NS_TEST_ASSERT_MSG_EQ (video.c_size, 100, "ChunkSize");
-		NS_TEST_ASSERT_MSG_EQ (video.c_attributes_size, 10, "ChunkAttributeSize");
-	  }
-}
+    streaming::ChunkHeader msgOut;
+    packet.RemoveHeader(msgOut);
+    msgOut.Print(std::cout);
+      {
+        NS_TEST_ASSERT_MSG_EQ(msgOut.GetType(), MSG_PULL, "ChunkHeader Type");
+        NS_TEST_ASSERT_MSG_EQ(msgOut.GetReserved(), 1, "Reserved");
+        NS_TEST_ASSERT_MSG_EQ(msgOut.GetChecksum(), 65535, "Checksum");
+        streaming::ChunkHeader::PullMessage &chunkOut = msgIn.GetPullMessage();
+          {
+            NS_TEST_ASSERT_MSG_EQ(chunkOut.GetChunk(), 5, "ChunkIdentifier PULL");
+            chunkOut.Print(std::cout);
+          }
+      }
+  }
 
-class PullTestCase : public TestCase {
-public:
-	PullTestCase ();
-  virtual void DoRun (void);
-};
+  class HelloTestCase : public TestCase
+  {
+    public:
+      HelloTestCase ();
+      virtual void
+      DoRun (void);
+  };
 
-PullTestCase::PullTestCase ()
-  : TestCase ("Check PullMessage")
-{}
-void
-PullTestCase::DoRun (void)
-{
-	  Packet packet;
-	  streaming::ChunkHeader msgIn;
-	  msgIn.SetType(MSG_PULL);
-	  msgIn.SetReserved(1);
-	  msgIn.SetChecksum(65535);
-	  msgIn.Print(std::cout);
-	  {
-	    streaming::ChunkHeader::PullMessage &chunkIn = msgIn.GetPullMessage ();
-	    chunkIn.SetChunk(5);
-	    chunkIn.Print(std::cout);
-	  }
-	  packet.AddHeader(msgIn);
-
-	  streaming::ChunkHeader msgOut;
-	  packet.RemoveHeader (msgOut);
-	  msgOut.Print(std::cout);
-	  {
-	  NS_TEST_ASSERT_MSG_EQ(msgOut.GetType(),MSG_PULL,"ChunkHeader Type");
-	  NS_TEST_ASSERT_MSG_EQ(msgOut.GetReserved(),1,"Reserved");
-	  NS_TEST_ASSERT_MSG_EQ(msgOut.GetChecksum(),65535,"Checksum");
-	  streaming::ChunkHeader::PullMessage &chunkOut = msgIn.GetPullMessage ();
-	  {
-		  NS_TEST_ASSERT_MSG_EQ (chunkOut.GetChunk(), 5, "ChunkIdentifier PULL");
-		  chunkOut.Print(std::cout);
-	  }
-	  }
-}
-
-class HelloTestCase : public TestCase {
-public:
-	HelloTestCase ();
-  virtual void DoRun (void);
-};
-
-HelloTestCase::HelloTestCase ()
-  : TestCase ("Check HelloMessage")
-{}
-void
-HelloTestCase::DoRun (void)
-{
-	  Packet packet;
-	  streaming::ChunkHeader msgIn;
-	  msgIn.SetType(MSG_HELLO);
-	  msgIn.SetReserved(2);
-	  msgIn.SetChecksum(65534);
-	  msgIn.Print(std::cout);
-	  {
-	    streaming::ChunkHeader::HelloMessage &chunkIn = msgIn.GetHelloMessage ();
-	    chunkIn.SetLastChunk (1223);
-	    chunkIn.SetChunksReceived (1023);
-	    chunkIn.SetChunksRatio (80);
+  HelloTestCase::HelloTestCase () :
+      TestCase("Check HelloMessage")
+  {
+  }
+  void
+  HelloTestCase::DoRun (void)
+  {
+    Packet packet;
+    streaming::ChunkHeader msgIn;
+    msgIn.SetType(MSG_HELLO);
+    msgIn.SetReserved(2);
+    msgIn.SetChecksum(65534);
+    msgIn.Print(std::cout);
+      {
+        streaming::ChunkHeader::HelloMessage &chunkIn = msgIn.GetHelloMessage();
+        chunkIn.SetLastChunk(1223);
+        chunkIn.SetChunksReceived(1023);
+        chunkIn.SetChunksRatio(80);
 //	    chunkIn.SetDestination (Ipv4Address("10.1.2.3"));
 //	    chunkIn.SetNeighborhoodSize (8);
-	    chunkIn.Print(std::cout);
-	  }
-	  packet.AddHeader(msgIn);
+        chunkIn.Print(std::cout);
+      }
+    packet.AddHeader(msgIn);
 
-	  streaming::ChunkHeader msgOut;
-	  packet.RemoveHeader (msgOut);
-	  msgOut.Print(std::cout);
-	  {
-	  NS_TEST_ASSERT_MSG_EQ(msgOut.GetType(),MSG_HELLO,"ChunkHeader Type");
-	  NS_TEST_ASSERT_MSG_EQ(msgOut.GetReserved(),2,"Reserved");
-	  NS_TEST_ASSERT_MSG_EQ(msgOut.GetChecksum(),65534,"Checksum");
-	  streaming::ChunkHeader::HelloMessage &chunkOut = msgIn.GetHelloMessage ();
-	  {
-		  NS_TEST_ASSERT_MSG_EQ (chunkOut.GetLastChunk(), 1223, "Last Chunk");
-		  NS_TEST_ASSERT_MSG_EQ (chunkOut.GetChunksReceived(), 1023, "Chunks Received");
-		  NS_TEST_ASSERT_MSG_EQ (chunkOut.GetChunksRatio(), 80, "Chunks Ratio");
+    streaming::ChunkHeader msgOut;
+    packet.RemoveHeader(msgOut);
+    msgOut.Print(std::cout);
+      {
+        NS_TEST_ASSERT_MSG_EQ(msgOut.GetType(), MSG_HELLO, "ChunkHeader Type");
+        NS_TEST_ASSERT_MSG_EQ(msgOut.GetReserved(), 2, "Reserved");
+        NS_TEST_ASSERT_MSG_EQ(msgOut.GetChecksum(), 65534, "Checksum");
+        streaming::ChunkHeader::HelloMessage &chunkOut = msgIn.GetHelloMessage();
+          {
+            NS_TEST_ASSERT_MSG_EQ(chunkOut.GetLastChunk(), 1223, "Last Chunk");
+            NS_TEST_ASSERT_MSG_EQ(chunkOut.GetChunksReceived(), 1023, "Chunks Received");
+            NS_TEST_ASSERT_MSG_EQ(chunkOut.GetChunksRatio(), 80, "Chunks Ratio");
 //		  NS_TEST_ASSERT_MSG_EQ (chunkOut.GetDestination(), Ipv4Address("10.1.2.3"), "Ip destination");
 //		  NS_TEST_ASSERT_MSG_EQ (chunkOut.GetNeighborhoodSize(), 8, "Neighborhood Size");
-		  chunkOut.Print(std::cout);
-	  }
-	  }
-}
-static class ChunkTestSuite : public TestSuite
-{
-public:
-	ChunkTestSuite ();
-} j_ChunkTestSuite;
+            chunkOut.Print(std::cout);
+          }
+      }
+  }
+  static class ChunkTestSuite : public TestSuite
+  {
+    public:
+      ChunkTestSuite ();
+  } j_ChunkTestSuite;
 
-ChunkTestSuite::ChunkTestSuite()
-  : TestSuite("chunk-header", UNIT)
-{
-  // RUN $ ./test.py -s chunk-header -v -c unit 1
-  AddTestCase(new ChunkHeaderTestCase());
-  AddTestCase(new ChunkTestCase());
-  AddTestCase(new PullTestCase());
-  AddTestCase(new HelloTestCase());
-}
+  ChunkTestSuite::ChunkTestSuite () :
+      TestSuite("chunk-header", UNIT)
+  {
+    // RUN $ ./test.py -s chunk-header -v -c unit 1
+    AddTestCase(new ChunkHeaderTestCase());
+    AddTestCase(new ChunkTestCase());
+    AddTestCase(new PullTestCase());
+    AddTestCase(new HelloTestCase());
+  }
 
 } // namespace ns3
 
