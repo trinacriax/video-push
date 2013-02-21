b0d2388939af03d55241394ad037546070cffdde
Alessandro Russo <russo@disi.unitn.it>
2013-02-21 14:08:45 +0100

    reformatting

--- model/chunk-buffer.cc
+++ model/chunk-buffer.cc
@@ -26,204 +26,204 @@
 #include <memory.h>
 #include "ns3/log.h"
 
-NS_LOG_COMPONENT_DEFINE ("ChunkBuffer");
+NS_LOG_COMPONENT_DEFINE("ChunkBuffer");
 
-namespace ns3{
+namespace ns3
+{
 
+  ChunkBuffer::ChunkBuffer () :
+      last(0)
+  {
+    chunk_buffer.clear();
+    chunk_state.clear();
+  }
 
-	ChunkBuffer::ChunkBuffer () :
-			last(0)
-	{
-		chunk_buffer.clear();
-		chunk_state.clear();
-	}
+  ChunkBuffer::~ChunkBuffer ()
+  {
+    chunk_buffer.clear();
+    chunk_state.clear();
+  }
 
-	ChunkBuffer::~ChunkBuffer ()
-	{
-		chunk_buffer.clear();
-		chunk_state.clear();
-	}
+  ChunkVideo*
+  ChunkBuffer::GetChunk (uint32_t chunkid)
+  {
+    NS_ASSERT(chunkid>0);
+    ChunkVideo *copy = 0;
+    std::map<uint32_t, ChunkVideo>::iterator const result = chunk_buffer.find(chunkid);
+    if (result != chunk_buffer.end())
+      copy = &(result->second);
+    return copy;
+  }
 
-	ChunkVideo*
-	ChunkBuffer::GetChunk (uint32_t chunkid)
-	{
-		NS_ASSERT (chunkid>0);
-		ChunkVideo *copy = 0;
-		std::map<uint32_t, ChunkVideo>::iterator const result = chunk_buffer.find(chunkid);
-		if (result != chunk_buffer.end())
-			copy = &(result->second);
-		return copy;
-	}
+  bool
+  ChunkBuffer::HasChunk (uint32_t chunkid)
+  {
+    NS_ASSERT(chunkid>0);
+    std::map<uint32_t, ChunkVideo>::iterator const result = chunk_buffer.find(chunkid);
+    if (result != chunk_buffer.end())
+      return true;
+    return false;
+  }
 
-	bool
-	ChunkBuffer::HasChunk (uint32_t chunkid)
-	{
-		NS_ASSERT (chunkid>0);
-		std::map<uint32_t, ChunkVideo>::iterator const result = chunk_buffer.find(chunkid);
-		if (result != chunk_buffer.end())
-			return true;
-		return false;
-	}
+  bool
+  ChunkBuffer::AddChunk (const ChunkVideo &chunk, ChunkState state)
+  {
+    NS_ASSERT(state==CHUNK_RECEIVED_PUSH||state==CHUNK_RECEIVED_PULL);
+    std::map<uint32_t, ChunkVideo>::iterator result = chunk_buffer.find(chunk.c_id);
+    if (result == chunk_buffer.end())
+      {
+        std::pair<uint32_t, ChunkVideo> entry(chunk.c_id, chunk);
+        chunk_buffer.insert(entry);
+        state = (chunk_state.find(chunk.c_id) == chunk_state.end() ? state : CHUNK_RECEIVED_PULL);
+        std::pair<uint32_t, ChunkState> sentry(chunk.c_id, state);
+        chunk_state.insert(sentry);
+        last = (chunk.c_id > last) ? chunk.c_id : last;
+        return true;
+      }
+    return false;
+  }
 
-	bool
-	ChunkBuffer::AddChunk (const ChunkVideo &chunk, ChunkState state)
-	{
-		NS_ASSERT (state==CHUNK_RECEIVED_PUSH||state==CHUNK_RECEIVED_PULL);
-		std::map<uint32_t, ChunkVideo>::iterator result = chunk_buffer.find(chunk.c_id);
-		if (result == chunk_buffer.end()){
-			std::pair <uint32_t, ChunkVideo> entry (chunk.c_id, chunk);
-			chunk_buffer.insert(entry);
-			state = (chunk_state.find(chunk.c_id)==chunk_state.end()?state:CHUNK_RECEIVED_PULL);
-			std::pair <uint32_t, ChunkState> sentry (chunk.c_id, state);
-			chunk_state.insert(sentry);
-			last = (chunk.c_id > last) ? chunk.c_id : last;
-			return true;
-		}
-		return false;
-	}
+  bool
+  ChunkBuffer::DelChunk (uint32_t chunkid)
+  {
+    NS_ASSERT(chunkid>0);
+    return chunk_buffer.erase(chunkid);
+  }
 
-	bool
-	ChunkBuffer::DelChunk(uint32_t chunkid)
-	{
-		NS_ASSERT (chunkid>0);
-		return chunk_buffer.erase(chunkid);
-	}
+  const size_t
+  ChunkBuffer::GetBufferSize ()
+  {
+    return chunk_buffer.size();
+  }
 
-	const size_t
-	ChunkBuffer::GetBufferSize ()
-	{
-		return chunk_buffer.size();
-	}
+  std::map<uint32_t, ChunkVideo>
+  ChunkBuffer::GetChunkBuffer ()
+  {
+    return chunk_buffer;
+  }
 
-	std::map<uint32_t, ChunkVideo>
-	ChunkBuffer::GetChunkBuffer()
-	{
-		return chunk_buffer;
-	}
+  std::string
+  ChunkBuffer::PrintBuffer ()
+  {
+    std::stringstream buf;
+    for (std::map<uint32_t, ChunkVideo>::iterator iter = chunk_buffer.begin(); iter != chunk_buffer.end(); iter++)
+      {
+        buf << iter->first << ", ";
+      }
+    return buf.str();
+  }
 
-	std::string
-	ChunkBuffer::PrintBuffer()
-	{
-		std::stringstream buf;
-		for(std::map<uint32_t, ChunkVideo>::iterator iter = chunk_buffer.begin();
-			iter != chunk_buffer.end() ; iter++){
-			buf<<iter->first<<", ";
-		}
-		return buf.str();
-	}
+  bool
+  ChunkBuffer::ChunkSkipped (uint32_t chunkid)
+  {
+    NS_ASSERT(chunkid>0);
+    bool ret = (chunk_state.find(chunkid) != chunk_state.end() && chunk_state.find(chunkid)->second == CHUNK_SKIPPED);
+    return ret;
+  }
 
-	bool
-	ChunkBuffer::ChunkSkipped (uint32_t chunkid)
-	{
-		NS_ASSERT (chunkid>0);
-		bool ret = (chunk_state.find(chunkid) != chunk_state.end() && chunk_state.find(chunkid)->second == CHUNK_SKIPPED);
-		return ret;
-	}
+  bool
+  ChunkBuffer::ChunkMissed (uint32_t chunkid)
+  {
+    NS_ASSERT(chunkid>0);
+    bool ret = (chunk_buffer.find(chunkid) == chunk_buffer.end());
+    return ret;
+  }
 
-	bool
-	ChunkBuffer::ChunkMissed (uint32_t chunkid)
-	{
-		NS_ASSERT (chunkid>0);
-		bool ret = (chunk_buffer.find(chunkid) == chunk_buffer.end());
-		return ret;
-	}
+  bool
+  ChunkBuffer::ChunkDelayed (uint32_t chunkid)
+  {
+    NS_ASSERT(chunkid>0);
+    bool ret = (chunk_state.find(chunkid) != chunk_state.end() && chunk_state.find(chunkid)->second == CHUNK_DELAYED);
+    return ret;
+  }
 
-	bool
-	ChunkBuffer::ChunkDelayed (uint32_t chunkid)
-	{
-		NS_ASSERT (chunkid>0);
-		bool ret = (chunk_state.find(chunkid) != chunk_state.end() && chunk_state.find(chunkid)->second == CHUNK_DELAYED);
-		return ret;
-	}
+  uint32_t
+  ChunkBuffer::GetLatestMissed (uint32_t base, uint32_t window)
+  {
+    uint32_t missed = (base + window <= last ? base + window : last);
+    uint32_t low = base;
+    low = low < 1 ? 1 : low;
+    while (missed >= low && (HasChunk(missed) || ChunkSkipped(missed) || ChunkDelayed(missed)))
+      {
+        missed--;
+      }
+    missed = (missed <= last ? missed : 0);
+    missed = (missed >= low ? missed : 0);
+    return missed;
+  }
 
-	uint32_t
-	ChunkBuffer::GetLatestMissed (uint32_t base, uint32_t window)
-	{
-		uint32_t missed = (base+window <= last ? base+window : last );
-		uint32_t low = base;
-		low = low < 1 ? 1 : low;
-		while (missed >= low && (HasChunk(missed) || ChunkSkipped (missed) || ChunkDelayed (missed)))
-		{
-			missed--;
-		}
-		missed = (missed<=last?missed:0);
-		missed = (missed>=low?missed:0);
-		return missed;
-	}
+  uint32_t
+  ChunkBuffer::GetLeastMissed (uint32_t base, uint32_t window)
+  {
+    NS_ASSERT(base >=0 && window > 0);
+    uint32_t missed = 1;
+    uint32_t low = base;
+    missed = low < 1 ? 1 : low;
+    uint32_t upper = (base + window <= last ? base + window : last);
+    while (missed <= upper && (HasChunk(missed) || ChunkSkipped(missed) || ChunkDelayed(missed)))
+      {
+        missed++;
+      }
+    missed = (missed <= upper ? missed : 0);
+    missed = (missed >= low ? missed : 0);
+    return missed;
+  }
 
-	uint32_t
-	ChunkBuffer::GetLeastMissed (uint32_t base, uint32_t window)
-	{
-		NS_ASSERT (base >=0 && window > 0);
-		uint32_t missed = 1;
-		uint32_t low = base;
-		missed = low < 1 ? 1 : low;
-		uint32_t upper = (base+window <= last ? base+window : last );
-		while (missed <= upper && (HasChunk(missed) || ChunkSkipped (missed) || ChunkDelayed (missed)))
-		{
-			missed++;
-		}
-		missed = (missed<=upper?missed:0);
-		missed = (missed>=low?missed:0);
-		return missed;
-	}
+  uint32_t
+  ChunkBuffer::GetLastChunk ()
+  {
+    return last;
+  }
 
-	uint32_t
-	ChunkBuffer::GetLastChunk ()
-	{
-		return last;
-	}
+  uint32_t
+  ChunkBuffer::GetSize ()
+  {
+    return chunk_buffer.size();
+  }
 
-	uint32_t
-	ChunkBuffer::GetSize ()
-	{
-		return chunk_buffer.size();
-	}
+  void
+  ChunkBuffer::SetChunkState (uint32_t chunkid, ChunkState state)
+  {
+    if (chunk_state.find(chunkid) == chunk_state.end())
+      chunk_state.insert(std::pair<uint32_t, ChunkState>(chunkid, state));
+    switch (state)
+      {
+      case CHUNK_RECEIVED_PULL:
+      case CHUNK_RECEIVED_PUSH:
+        {
+          NS_ASSERT(HasChunk(chunkid));
+          chunk_state.find(chunkid)->second = state;
+          break;
+        }
+      case CHUNK_MISSED:
+      case CHUNK_SKIPPED:
+      case CHUNK_DELAYED:
+        {
+          NS_ASSERT(!HasChunk(chunkid));
+          chunk_state.find(chunkid)->second = state;
+          NS_ASSERT(ChunkSkipped(chunkid)||ChunkMissed(chunkid)||ChunkDelayed(chunkid));
+          break;
+        }
+      default:
+        {
+          NS_ASSERT(true);
+          break;
+        }
+      }
+  }
 
-	void
-	ChunkBuffer::SetChunkState (uint32_t chunkid, ChunkState state)
-	{
-		if (chunk_state.find(chunkid) == chunk_state.end())
-			chunk_state.insert(std::pair <uint32_t, ChunkState> (chunkid, state));
-		switch (state)
-		{
-			case CHUNK_RECEIVED_PULL:
-			case CHUNK_RECEIVED_PUSH:
-			{
-				NS_ASSERT(HasChunk(chunkid));
-				chunk_state.find(chunkid)->second = state;
-				break;
-			}
-			case CHUNK_MISSED:
-			case CHUNK_SKIPPED:
-			case CHUNK_DELAYED:
-			{
-				NS_ASSERT(!HasChunk(chunkid));
-				chunk_state.find(chunkid)->second = state;
-				NS_ASSERT (ChunkSkipped(chunkid)||ChunkMissed(chunkid)||ChunkDelayed(chunkid));
-				break;
-			}
-			default:
-			{
-				NS_ASSERT (true);
-				break;
-			}
-		}
-	}
-
-	ChunkState
-	ChunkBuffer::GetChunkState (uint32_t chunkid)
-	{
-		NS_ASSERT (chunkid>0);
-		std::map<uint32_t, ChunkState>::iterator iter = chunk_state.find(chunkid);
-		if (iter == chunk_state.end())
-		{
-			return CHUNK_MISSED;
-		}
-		else
-			return iter->second;
-	}
+  ChunkState
+  ChunkBuffer::GetChunkState (uint32_t chunkid)
+  {
+    NS_ASSERT(chunkid>0);
+    std::map<uint32_t, ChunkState>::iterator iter = chunk_state.find(chunkid);
+    if (iter == chunk_state.end())
+      {
+        return CHUNK_MISSED;
+      }
+    else
+      return iter->second;
+  }
 
 }
-
 
