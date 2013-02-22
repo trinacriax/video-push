51345118055e7469d40f5db2b27d1e74b4778278
Alessandro Russo <russo@disi.unitn.it>
2013-02-22 17:48:56 +0100

    Minor changes in the GPL description at the beginning of each file

--- examples/video-streaming-pimdm.cc
+++ examples/video-streaming-pimdm.cc
@@ -1,6 +1,6 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2009 IITP RAS
+ * Copyright (c) 2011 University of Trento, Italy
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -15,8 +15,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Authors : Alessandro Russo <russo@disi.unitn.it>
  *
+ * Authors: Alessandro Russo <russo@disi.unitn.it>
+ *          University of Trento, Italy
  */
 
 #include <iostream>
